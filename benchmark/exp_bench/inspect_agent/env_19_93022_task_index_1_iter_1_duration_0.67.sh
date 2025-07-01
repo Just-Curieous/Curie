bf1b3da1765b842de000250337ed281a5b87eed0
crakeee <crake408@gmail.com>
2025-07-01 02:03:06 -0400

    fixed curie

--- benchmark/exp_bench/inspect_agent/env_19_93022_task_index_1_iter_1_duration_0.67.sh
+++ benchmark/exp_bench/inspect_agent/env_19_93022_task_index_1_iter_1_duration_0.67.sh
@@ -9,16 +9,8 @@ export PROMPT_FILE_PATH=19_93022_task_index_1_iter_1_duration_0.67_agent_prompt.
 
 # Azure OPENAI API key setup example:
 # set your model
-export MODEL="openai/o3-mini"
-# set up your API key credential
-export AZURE_API_KEY="38bbef1cde894a7aa89bd7e29d1a0976"
-export AZURE_API_BASE="https://api.umgpt.umich.edu/azure-openai-api"
-export AZURE_API_VERSION="2024-06-01"
-export ORGANIZATION='014482'
-export MODEL=openai/o3-mini
-export AZUREAI_OPENAI_API_KEY=38bbef1cde894a7aa89bd7e29d1a0976
-export AZUREAI_OPENAI_API_BASE=https://api.umgpt.umich.edu/azure-openai-api
-export AZUREAI_OPENAI_API_VERSION=2024-06-01
-export OPENAI_API_KEY=$AZUREAI_OPENAI_API_KEY
-export OPENAI_API_BASE=$AZUREAI_OPENAI_API_BASE
-export OPENAI_API_VERSION=$AZUREAI_OPENAI_API_VERSION
+export MODEL="azure/gpt-4o"
+export AZURE_API_VERSION="2025-01-01-preview"
+export AZURE_API_KEY="bdef9bf150194da5b8ad860046aff989"
+export ORGANIZATION='327403'
+export AZURE_API_BASE='https://api.umgpt.umich.edu/azure-openai-api'
--- build/lib/curie/model.py
+++ build/lib/curie/model.py
@@ -3,8 +3,6 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 import tiktoken
 import time
 import os
-import litellm  
-litellm.drop_params = True #
 from openai import BadRequestError
 from typing import List, Dict, Any
 from langchain_core.messages import BaseMessage, HumanMessage, SystemMessage
